345fb9aceb37fd9258b3f3c34c85390e1fef2bd1
Ciprian Manea <ciprian-terraindex@netrom.ro>
2025-06-02 21:42:33 +0300

    fix for build

--- src/composables/AuthentificationAPI.ts
+++ src/composables/AuthentificationAPI.ts
@@ -1,21 +1,29 @@
 import { call } from '@/composables/HTTPClient.js';
+import { useRouter } from 'vue-router'
+import { useToast } from 'vue-toastification'
 
 export function useAuth() {
+  const router = useRouter();
+  const toast = useToast();
 
   async function login(credentials: { email: string; password: string }, authStore: any) {
     try {
-      const response = await call('get', `/login`, credentials);
-      authStore.setJwt(response.jwt);
+      const response = await call('post', `/authentication/login`, credentials);
+      authStore.setJwt(response.token);
+      router.push("/");
     } catch (error) {
+      toast.error(`Credentials are not valid`);
       throw new Error(`Login failed: ${error.message}`);
     }
   }
 
   async function register(credentials: { name: string; email: string; password: string }, authStore: any) {
     try {
-      const response = await call('post', `/register`, credentials);
-      authStore.setJwt(response.jwt);
+      await call('post', `/authentication/register`, credentials);
+      toast.success('Registration successful! Please log in.');
+      router.push("/login");
     } catch (error) {
+      toast.error(`${error}`)
       throw new Error(`Register failed: ${error.message}`);
     }
   }
